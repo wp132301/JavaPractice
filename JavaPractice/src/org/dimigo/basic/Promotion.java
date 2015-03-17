3d9e7f5ebcd26076caa82e79b9b7c8adbce01dab
Administrator <Administrator@MSDN-SPECIAL>
2015-03-17 15:12:14 +0900

    ..

---
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,10 @@
+package org.dimigo.basic;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("월 평균 급여 : ");
+	}
+
+}
