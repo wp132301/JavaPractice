b15dfab7d8f3a7e42b7cbc5e04e4ca78986ea5bc
Administrator <Administrator@MSDN-SPECIAL>
2015-03-18 14:58:13 +0900

    과제 제출

--- JavaPractice/src/org/dimigo/basic/Promotion.java
+++ JavaPractice/src/org/dimigo/basic/Promotion.java
@@ -4,7 +4,17 @@ public class Promotion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("월 평균 급여 : ");
+		int month = 1700000;
+		int parttime = 3;
+		int count = 1500;
+		long year = (long)month*12*parttime*count;
+		
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : " + String.format("%,d", month) + "원");
+		System.out.println("점포 내 직원 수 : " + parttime + "명");
+		System.out.println("점포 수 : " + String.format("%,d", count) + "개");
+		System.out.println("연간 인건비 : " +  String.format("%,d", year) + "원");
+		
 	}
 
 }
