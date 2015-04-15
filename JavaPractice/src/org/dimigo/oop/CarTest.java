771ab9162550227606996045c273146af67f4743
Administrator <Administrator@MSDN-SPECIAL>
2015-04-15 14:16:49 +0900

    실습과제 7-2

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -17,24 +17,26 @@ package org.dimigo.oop;
  */
 public class CarTest {
 	public static void main(String[] args) {
-		Car car[] = {new Car(), new Car(), new Car()};
-		car[0].setCompany("현대자동차");
-		car[0].setModel("제네시스");
-		car[0].setColor("검정색");
-		car[0].setMaxSpeed(225);
-		car[0].setPrice(50000000);
-		
-		car[1].setCompany("기아자동차");
-		car[1].setModel("K7");
-		car[1].setColor("흰색");
-		car[1].setMaxSpeed(246);
-		car[1].setPrice(40000000);
-		
-		car[2].setCompany("삼성자동차");
-		car[2].setModel("SM7");
-		car[2].setColor("회색");
-		car[2].setMaxSpeed(200);
-		car[2].setPrice(38000000);
+//		Car car[] = {new Car(), new Car(), new Car()};
+//		car[0].setCompany("현대자동차");
+//		car[0].setModel("제네시스");
+//		car[0].setColor("검정색");
+//		car[0].setMaxSpeed(225);
+//		car[0].setPrice(50000000);
+//		
+//		car[1].setCompany("기아자동차");
+//		car[1].setModel("K7");
+//		car[1].setColor("흰색");
+//		car[1].setMaxSpeed(246);
+//		car[1].setPrice(40000000);
+//		
+//		car[2].setCompany("삼성자동차");
+//		car[2].setModel("SM7");
+//		car[2].setColor("회색");
+//		car[2].setMaxSpeed(200);
+//		car[2].setPrice(38000000);
+//		
+		Car2 car[] = {new Car2("현대자동차", "제네시스", "검정색", 225, 50000000), new Car2("기아자동차", "K7", "흰색", 246, 40000000), new Car2("삼성자동차", "SM7", "회색", 200, 38000000)};
 		
 		System.out.println("제조사명 : " + car[0].getCompany());
 		System.out.println("모델명 : " + car[0].getModel());
